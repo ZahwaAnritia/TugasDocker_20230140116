fc955edade795c487880021ec7bc2cefbf0a6e17
ZahwaAnritia <anritia15@gmail.com>
2026-04-13 14:29:34 +0700

    menambahkan usercontroller dengan fitur login dan validasi

--- src/main/java/com/tugas/deploy/controller/UserController.java
+++ src/main/java/com/tugas/deploy/controller/UserController.java
@@ -1,4 +1,40 @@
 package com.tugas.deploy.controller;
 
+import com.tugas.deploy.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class UserController {
+
+
+    private static List<User> listMahasiswa = new ArrayList<>();
+
+
+    private final String NIM_SAYA = "20230140116";
+
+    @GetMapping("/")
+    public String redirectLogin() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String doLogin(@RequestParam String username, @RequestParam String password, Model model) {
+
+        if ("admin".equals(username) && NIM_SAYA.equals(password)) {
+            return "redirect:/home";
+        }
+        model.addAttribute("error", "Username atau Password salah!");
+        return "login";
+    }
+
 }
